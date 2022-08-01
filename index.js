798a637e66b2fc3865feb5a150fe028624f6cffe
Christian <chrisdaq@gmail.com>
2022-08-01 15:53:00 -0400

    add express package, create index.js to serve express server, use dotenv package to handle environment variables

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
+require("dotenv").config(); // use .env variables. access variables using "process.env"
 
 // create express server
 const app = express();
 
+// public dir
+app.use(express.static("public")); // serve static content instead of console.log
+
 // routes
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   // send json response
   res.json({
     ok: true,
   });
-});
+}); */
 
 // listen to petitions
-app.listen(4000, () => {
-  console.log("server running at port " + 4000);
+app.listen(process.env.PORT, () => {
+  console.log("server running at port " + process.env.PORT);
 });
