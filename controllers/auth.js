71e959b4f7f9479bdc2b55d03588f04211693dcd
Christian <chrisdaq@gmail.com>
2022-08-04 10:45:42 -0400

    add other routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,28 @@
-const createUser = (req, res) => {
+const { response } = require("express"); // to get autocomplete intellisense
+
+const createUser = (req, res = response) => {
+  // to get autocomplete intellisense
   res.json({
     ok: true,
+    msg: "register",
   });
 };
-// loginUser
-// renewToken
+const login = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "login",
+  });
+};
+
+const renewToken = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+};
 
 module.exports = {
   createUser,
+  login,
+  renewToken,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,13 @@
  */
 
 const { Router } = require("express");
-const { createUser } = require("../controllers/auth");
+const { createUser, login, renewToken } = require("../controllers/auth");
 const router = Router();
 
 router.post("/new", createUser);
+
+router.post("/", login);
+
+router.get("/renew", renewToken);
 
 module.exports = router; // node export system
