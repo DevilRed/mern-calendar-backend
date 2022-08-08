27ee7521970bd2a1e4e8379215cfd5624dfbde0d
Christian <chrisdaq@gmail.com>
2022-08-08 12:32:32 -0400

    use User model in auth controller, handling good and bad responses

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,25 @@
 const { response } = require("express"); // require express to get autocomplete intellisense
+const User = require("../models/User");
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
   // add response to get autocomplete intellisense
+  try {
+    // add new user object
+    const user = new User(req.body);
+    // save user
+    await user.save();
 
-  res.status(201).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+    res.status(201).json({
+      ok: true,
+      msg: "register",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Please contact with site administrator",
+    });
+  }
 };
 const login = (req, res = response) => {
   const { email, password } = req.body;
