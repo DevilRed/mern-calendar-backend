1e1749413a53c475852791083ab4a95ef570504a
Christian <chrisdaq@gmail.com>
2022-08-29 17:03:17 -0400

    installed cors package, use it with default config as middleware in app

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 require("dotenv").config(); // use .env variables. access variables using "process.env"
 const { dbConnection } = require("./database/config");
+const cors = require("cors");
 
 // create express server
 const app = express();
 
 // db connection
 dbConnection();
+
+// cors middleware, it's gonna be applied to all routes
+app.use(cors());
 
 // public dir
 app.use(express.static("public")); // serve static content instead of console.log
