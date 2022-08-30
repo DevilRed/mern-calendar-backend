e86ef981dec62b3e1d0656719f6f6111247a61c1
Christian <chrisdaq@gmail.com>
2022-08-30 10:30:01 -0400

    refactor validateJWT middleware, since all routes use it, validateJWT is put globally

--- routes/events.js
+++ routes/events.js
@@ -14,10 +14,11 @@ const {
 } = require("../controllers/events");
 
 const router = Router();
+router.use(validateJWT);
 
-router.get("/", validateJWT, getEvents);
-router.post("/", validateJWT, addEvent);
-router.put("/:id", validateJWT, updateEvent);
-router.delete("/:id", validateJWT, deleteEvent);
+router.get("/", getEvents);
+router.post("/", addEvent);
+router.put("/:id", updateEvent);
+router.delete("/:id", deleteEvent);
 
 module.exports = router;
