3d8d933036ad7c433c7cc3a6656d68ed06925c02
Christian <chrisdaq@gmail.com>
2022-09-02 16:11:28 -0400

    event delete endpoint done

--- controllers/events.js
+++ controllers/events.js
@@ -39,20 +39,20 @@ const updateEvent = async (req, res = response) => {
     const event = await Event.findById(eventId);
     // event not found
     if (!event) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: "Event not found",
       });
     }
     // user authorization for given event
     if (event.user.toString() !== uid) {
-      res.status(401).json({
+      return res.status(401).json({
         ok: false,
         msg: "Not enough privileges to update the event",
       });
     }
 
-    // desctructure all data sent in request
+    // destructure all data sent in request
     const newEvent = {
       ...req.body,
       user: uid, // update user id
@@ -62,13 +62,13 @@ const updateEvent = async (req, res = response) => {
     const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, {
       new: true,
     });
-    res.status(200).json({
-      ok: false,
+    return res.status(200).json({
+      ok: true,
       event: eventUpdated,
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       msg: "Talk with the administrator",
     });
@@ -76,10 +76,37 @@ const updateEvent = async (req, res = response) => {
 };
 
 const deleteEvent = async (req, res = response, id) => {
-  res.status(200).json({
-    ok: true,
-    msg: "deleteEvent",
-  });
+  // process almost identical as update
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const event = await Event.findById(eventId);
+    // event not found
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Event not found",
+      });
+    }
+    // user authorization for given event
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "Not enough privileges to delete the event",
+      });
+    }
+
+    const deleted = await Event.findByIdAndDelete(eventId);
+    return res.status(200).json({
+      ok: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Talk with the administrator",
+    });
+  }
 };
 
 module.exports = {
